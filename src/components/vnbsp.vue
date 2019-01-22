00104269dbe8c7dff9b238a684dfcdd392792d2c
kubarium <iam@denizkumsal.com>
2019-01-21 22:08:57 -0500

    nbsp component

---
+++ src/components/vnbsp.vue
@@ -0,0 +1,24 @@
+<template>
+  <span v-html="nbsp"></span>
+</template>
+
+<script>
+export default {
+  name: "v-nbsp",
+  props: {
+    count: {
+      type: Number,
+      default: 1,
+      validator: function(value) {
+        // The value must be higher than 0 match one of these strings
+        return value >= 1;
+      }
+    }
+  },
+  data() {
+    return {
+      nbsp: "&nbsp;".repeat(this.count)
+    };
+  }
+};
+</script>
